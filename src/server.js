2ba8439a07618cc46cc92f71776c92f2c18fa64a
mostafa-kamal <mmkamal021@gmail.com>
2023-03-13 20:04:51 +0600

    commited

--- src/server.js
+++ src/server.js
@@ -12,13 +12,77 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
-app.get('/id', async (req, res) => {
+app.delete('/:id', async (req, res) => {
+  const id = req.params.id
+
+  const data = await fs.readFile(dbLocation)
+  const players = JSON.parse(data)
+  let player = players.find((item) => item.id === id)
+
+  if (!players) {
+    return res.status(404).json({ message: 'Player Not Found' })
+  }
+
+  const newPlayers = players.filter((item) => item.id !== id)
+  await fs.writeFile(dbLocation, JSON.stringify(newPlayers))
+  res.status(203).send()
+})
+
+app.put('/:id', async (req, res) => {
+  const id = req.params.id
+
+  const data = await fs.readFile(dbLocation)
+  const players = JSON.parse(data)
+
+  let player = players.find((item) => item.id === id)
+
+  if (!player) {
+    player = {
+      ...req.body,
+      id: shortid.generate(),
+    }
+    players.push(player)
+  } else {
+    player.name = req.body.name
+    player.country = req.body.country
+    player.rank = req.body.rank
+  }
+
+  await fs.writeFile(dbLocation, JSON.stringify(players))
+  res.status(200).json(player)
+})
+
+app.patch('/:id', async (req, res) => {
   const id = req.params.id
 
   const data = await fs.readFile(dbLocation)
   const players = JSON.parse(data)
 
   const player = players.find((item) => item.id === id)
+
+  if (!player) {
+    return res.status(404).json({ message: 'Player Not Found' })
+  }
+
+  player.name = req.body.name || player.name
+  player.country -= req.body.country || player.country
+  player.rank = req.body.rank || player.rank
+
+  await fs.writeFile(dbLocation, JSON.stringify(players))
+  res.status(200).json(player)
+})
+
+app.get('/:id', async (req, res) => {
+  const id = req.params.id
+
+  const data = await fs.readFile(dbLocation)
+  const players = JSON.parse(data)
+
+  const player = players.find((item) => item.id === id)
+
+  if (!player) {
+    return res.status(404).json({ message: 'Player Not Found' })
+  }
 
   res.status(200).json(player)
 })
